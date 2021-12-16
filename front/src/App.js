55eff7c96d3e8de6bbdf0f85f3f91094417a960d
Tiago Araujo <tiagoaraujosud@gmail.com>
2021-12-15 23:49:04 -0300

    Creation of the menu os the top

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import React from 'react';
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Page1 from './Pages/Page1';
 import Page2 from './Pages/Page2';
 import Page3 from './Pages/Page3';
-
 
 const App = () => {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Page1 />} />
+      <div>
+        <h1>Oi</h1>
 
-        <Route path="/cadastro" element={<Page2 />} />
-        
-        <Route path="/login" element={<Page3 />} />
-      </Routes>
+        <Link to='/'>Home</Link>
+        <Link to='/cadastro'>Register</Link>
+        <Link to='/login'>Login</Link>
+
+        <Routes>
+          <Route path="/" exact element={<Page1 />} />
+
+          <Route path="/cadastro" element={<Page2 />} />
+
+          <Route path="/login" element={<Page3 />} />
+        </Routes>
+
+      </div>
     </Router>
   );
 }
