a1f33ecaa46cf54468c3091f10c9ac4c0a51c68d
Tiago Araujo <tiagoaraujosud@gmail.com>
2021-12-21 19:47:10 -0300

    A new Menu as a component was created and the routes were changed

--- front/src/App.js
+++ front/src/App.js
@@ -1,32 +1,17 @@
 import './App.css';
 import React from 'react';
 
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Page1 from './Pages/Page1';
-import Page2 from './Pages/Page2';
-import Page3 from './Pages/Page3';
+import Menu from './Components/Menu';
 
 const App = () => {
 
   return (
-    <Router>
       <div>
-        <h1>Oi</h1>
+        <h1>Web-ecommerce</h1>
 
-        <Link to='/'>Home</Link>
-        <Link to='/cadastro'>Register</Link>
-        <Link to='/login'>Login</Link>
-
-        <Routes>
-          <Route path="/" exact element={<Page1 />} />
-
-          <Route path="/cadastro" element={<Page2 />} />
-
-          <Route path="/login" element={<Page3 />} />
-        </Routes>
+        <Menu />
 
       </div>
-    </Router>
   );
 }
 
