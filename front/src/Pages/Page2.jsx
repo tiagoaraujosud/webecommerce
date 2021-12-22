0de17dea18711d20c2c58369b765d3cad97d67ec
Tiago Araujo <tiagoaraujosud@gmail.com>
2021-12-22 00:31:01 -0300

    Added the Register and the login pages. Must create database link

--- front/src/Pages/Page2.jsx
+++ front/src/Pages/Page2.jsx
@@ -1,12 +1,66 @@
 import React from 'react';
 import './Page.css';
 
-const Page2 = () => {
-    return(
-        <div class = 'Page2'>
-            Cadastro
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as yup from 'yup';
+
+function Page2 () {
+    
+    const handleClickRegister = (values) => console.log.values;
+
+    const validationRegister = yup.object().shape({
+        email: yup.string().email("Não é um email válido").required("Este campo é obrigatório!"),
+        password: yup.string().min(8, "A senha deve ter pelo menos 8 caracteres.").required("Este campo é obrigatório!"),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null], "As senhas não são iguais!")
+    })
+
+    return (
+        
+        <div>
+        
+        <div className="container">
+            <h1>Registro</h1>
+            <Formik initialValues={{}} onSubmit={handleClickRegister} validationSchema={validationRegister}>
+
+            <Form className='login-form'>
+
+                <div className='login-form-group'>
+                
+                <Field name='email' className='form-field' placeHolder='email'></Field>
+
+                <ErrorMessage component='spam' name='email' className='form-error'></ErrorMessage>
+
+                </div>
+
+
+                <div className='login-form-group'>
+                
+                <Field name='password' className='form-field' placeHolder='senha'></Field>
+
+                <ErrorMessage component='spam' name='password' className='form-error'></ErrorMessage>
+
+                </div>
+
+                <div className='login-form-group'>
+                
+                <Field name='confirmPassword' className='form-field' placeHolder='Confirme sua senha'></Field>
+
+                <ErrorMessage component='spam' name='confirmPassword' className='form-error'></ErrorMessage>
+
+                </div>
+
+                <button className='button' type='submit'>Registrar</button>
+
+            </Form>
+
+            </Formik>
+
+        
         </div>
-    )
+
+        </div>
+    );
+
 
 }
 
--- front/src/Pages/Page3.jsx
+++ front/src/Pages/Page3.jsx
@@ -1,12 +1,54 @@
-import React from 'react';
 import './Page.css';
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as yup from 'yup';
 
-const Page3 = () => {
-    return(
-        <div class = 'Page3'>
-            Login
+
+function Page3() {
+
+    const handleClickLogin = (values) => console.log.values;
+
+    const validationLogin = yup.object().shape({
+        email: yup.string().email("Não é um email válido").required("Este campo é obrigatório!"),
+        password: yup.string().min(8, "A senha deve ter pelo menos 8 caracteres.").required("Este campo é obrigatório!")
+
+    });
+
+    return (
+        
+        <div>
+            <div className="container">
+                <h1>Login</h1>
+                <Formik initialValues={{}} onSubmit={handleClickLogin} validationSchema={validationLogin}>
+
+                <Form className='login-form'>
+
+                    <div className='login-form-group'>
+                    
+                    <Field name='email' className='form-field' placeHolder='email'></Field>
+
+                    <ErrorMessage component='spam' name='email' className='form-error'></ErrorMessage>
+
+                    </div>
+
+
+                    <div className='login-form-group'>
+                    
+                    <Field name='password' className='form-field' placeHolder='senha'></Field>
+
+                    <ErrorMessage component='spam' name='password' className='form-error'></ErrorMessage>
+
+                    </div>
+
+                    <button className='button' type='submit' >Login</button>
+
+                </Form>
+
+                </Formik>
+            </div>
+        
         </div>
-    )
+    );
 }
 
 export default Page3;
