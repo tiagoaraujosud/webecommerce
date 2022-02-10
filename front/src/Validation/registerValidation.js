15ec4ad5785c5512bc0890cc0d01ca412191f051
Marcus Vinicius Leite <aci.marcus@gmail.com>
2022-02-10 17:57:07 -0300

    refactor: yup validation of register page

--- front/src/Validation/registerValidation.js
+++ front/src/Validation/registerValidation.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const registerSchema = Yup.object().shape({
     email: Yup.string().email().required('Required'),
-    password: Yup.string().min(4).max(5).required()
+    password: Yup.string().min(6).max(12).required('Required')
 });
 
 export default registerSchema;
