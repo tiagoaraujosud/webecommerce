6a4736a09f7cb3bea980b597334a62e13c52dcb2
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-10-10 23:36:01 -0300

    Changing files name to index

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
 
-import Menu from '../Components/Menu/Menu';
-import Home from '../Pages/home/Home';
-import Register from '../Pages/register/Register';
-import Login from '../Pages/login/Login';
-import Products from '../Pages/products/Products';
+import Menu from '../Components/menu';
+import Home from '../Pages/home';
+import Register from '../Pages/register';
+import Login from '../Pages/login';
+import Products from '../Pages/products';
 
 const Routes = () => {
   return(
