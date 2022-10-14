239ec5982e596844ea0db16e0d2f36404f04a5a7
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-10-13 21:43:00 -0300

    Flexbox: footer

--- front/src/Components/footer/index.jsx
+++ front/src/Components/footer/index.jsx
@@ -8,9 +8,9 @@ function Footer() {
     <>
     <nav>
       <ul class='footer'>
-        <li class="logo"><a href="#"><img src='img/facebook.png'></img>Facebook</a></li>
-        <li class="logo"><a href="#">Instagram</a></li>
-        <li class="logo"><a href="">Whatsapp</a></li>
+        <li class="logo"><a href="http://www.facebook.com"><img src='https://cdn-icons-png.flaticon.com/512/20/20673.png'></img></a></li>
+        <li class="logo"><a href="http://www.instagram.com"><img src='https://imagensemoldes.com.br/wp-content/uploads/2020/07/Logo-do-Instagram-PNG-1280x720.png'></img></a></li>
+        <li class="logo"><a href=""><img src='https://assets.stickpng.com/images/5a019b697ca233f48ba6275a.png'></img></a></li>
       </ul>  
     </nav>
     </>
